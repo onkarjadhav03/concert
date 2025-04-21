c38d68292f60aefe6b45467733c9be4f35ed7723
onkar <jadhavonkar333@gmail.com>
2025-04-21 15:53:21 +0530

    files changed

--- config/config.js
+++ config/config.js
@@ -8,8 +8,8 @@ module.exports=
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    dialect: "postgres" 
-  },
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+},
 
 }
